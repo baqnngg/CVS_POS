dde9722c1b4349a1e9737c7ec4309b093186507d
baqnngg <il8696546@gmail.com>
2026-04-17 22:45:09 +0900

    inventory_file

--- Convenience_Store.py
+++ Convenience_Store.py
@@ -1,3 +1,15 @@
+import json
+
+INVENTORY_FILE = "inventory.json"
+
+def load_inventory():
+    with open(INVENTORY_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_inventory(data):
+    with open(INVENTORY_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
 # 재고 확인 함수
 def check_inventory(): return
 
@@ -19,15 +31,8 @@ def get_best_seller(): return
 # 일일 매출 리포트 출력 함수
 def print_daily_report(): return
 
-# 상품 재고
-inventory = {
-    "P001": {
-        "name": "유기농 사과",
-        "price": 3000,
-        "stock": 50,
-        "category": "식품"
-    },
-}
+# 상품 재고 
+inventory = load_inventory()
 
 # 카테고리별 매출 집계
 sales_by_category = {}
